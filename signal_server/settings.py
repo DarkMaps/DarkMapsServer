5cd32e50ec8c24c7214e867510d096b5804b2635
Matthew Roche <matthew@matthewroche.co.uk>
2021-01-03 10:26:44 +0000

    Email updates

--- signal_server/settings.py
+++ signal_server/settings.py
@@ -207,6 +207,8 @@ DJOSER = {
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST', None)
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', None)
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', None)
 EMAIL_PORT = os.environ.get('EMAIL_PORT', None)
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', None)
 EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL', None)
