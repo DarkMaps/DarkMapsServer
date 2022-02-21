78c9b620127123a71219b7d35869b8c2bc204d5f
Matthew Roche <matthew@matthewroche.co.uk>
2022-02-21 10:00:58 +0000

    Chages for Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dark_maps.wsgi
--- dark_maps/settings.py
+++ dark_maps/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 from boto3.session import Session
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -314,3 +315,6 @@ if CLOUDWATCH_AWS_ID != None:
             "watchtower": {"level": "INFO", "handlers": ["watchtower"], "propogate": False,}
         },
     }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
